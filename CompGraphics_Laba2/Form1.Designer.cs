4031aade7b989a457cd1213a9f72e8b9cce91ea5
User <User@WIN-AK2SGH7E72N>
2020-09-11 21:33:36 +0300

    Добавьте файлы проекта.

---
+++ CompGraphics_Laba2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CompGraphics_Laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPixel = new System.Windows.Forms.Button();
+            this.btnMills = new System.Windows.Forms.Button();
+            this.Inches = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(20, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnPixel
+            // 
+            this.btnPixel.Location = new System.Drawing.Point(88, 480);
+            this.btnPixel.Name = "btnPixel";
+            this.btnPixel.Size = new System.Drawing.Size(100, 50);
+            this.btnPixel.TabIndex = 1;
+            this.btnPixel.Text = "Pixels";
+            this.btnPixel.UseVisualStyleBackColor = true;
+            this.btnPixel.Click += new System.EventHandler(this.btnPixel_Click);
+            // 
+            // btnMills
+            // 
+            this.btnMills.Location = new System.Drawing.Point(288, 480);
+            this.btnMills.Name = "btnMills";
+            this.btnMills.Size = new System.Drawing.Size(100, 50);
+            this.btnMills.TabIndex = 2;
+            this.btnMills.Text = "Millimeters";
+            this.btnMills.UseVisualStyleBackColor = true;
+            this.btnMills.Click += new System.EventHandler(this.btnMills_Click);
+            // 
+            // Inches
+            // 
+            this.Inches.Location = new System.Drawing.Point(546, 480);
+            this.Inches.Name = "Inches";
+            this.Inches.Size = new System.Drawing.Size(100, 50);
+            this.Inches.TabIndex = 3;
+            this.Inches.Text = "Inches";
+            this.Inches.UseVisualStyleBackColor = true;
+            this.Inches.Click += new System.EventHandler(this.Inches_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(748, 480);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 50);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 553);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.Inches);
+            this.Controls.Add(this.btnMills);
+            this.Controls.Add(this.btnPixel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPixel;
+        private System.Windows.Forms.Button btnMills;
+        private System.Windows.Forms.Button Inches;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ CompGraphics_Laba2/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompGraphics_Laba2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+
+            g.Clear(Color.FromArgb(254, 254, 254));
+        }
+
+        private void btnPixel_Click(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+
+            int ex = 0, ey = 0;
+            float old_ex = 0, old_ey = 0;
+            float x = 0, y = 0;
+            Pen axesPen = new Pen(Color.Red, 1);
+            Pen graphicsPen = new Pen(Color.FromArgb(0, 255, 0), 1);
+
+            pictureBox1.BackColor = Color.FromName("Azure");
+            pictureBox1.Refresh();
+
+            g.PageUnit = GraphicsUnit.Pixel;
+
+            g.DrawRectangle(axesPen, 0, 0, pictureBox1.Size.Width - 1,
+                pictureBox1.Size.Height - 1);
+            g.DrawLine(axesPen, 0, (pictureBox1.Size.Height - 1) / 2,
+                pictureBox1.Size.Width - 1, (pictureBox1.Size.Height - 1) / 2);
+            g.DrawLine(axesPen, (pictureBox1.Size.Width - 1) / 2, 0,
+                (pictureBox1.Size.Width - 1) / 2, pictureBox1.Size.Height - 1);
+
+            g.TranslateTransform((pictureBox1.Size.Width - 1)/2,
+                (pictureBox1.Size.Height - 1) / 2);
+            g.ScaleTransform(1, -1);
+
+            x = -40f;
+            while(x <= 40)
+            {
+                y = Convert.ToSingle(2 * Math.Pow(x, 3) + 2 * x);
+                g.DrawLine(graphicsPen, old_ex, old_ey, x, y);
+
+                old_ex = x;
+                old_ey = y;
+
+                x += 0.01f;
+            }
+
+            //g.TranslateTransform(0, 0);
+            g.ScaleTransform(-1, 1);
+
+            g.Dispose();
+        }
+
+        private void btnMills_Click(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+
+            int ex = 0, ey = 0;
+            float old_ex = 0, old_ey = 0;
+            float x = 0, y = 0;
+
+            g.PageUnit = GraphicsUnit.Millimeter;
+            Pen axesPen = new Pen(Color.Cyan, 0.1f);
+            Pen graphicsPen = new Pen(Color.FromArgb(0, 0, 255), 0.1f);
+
+            pictureBox1.BackColor = Color.FromKnownColor(KnownColor.ControlLightLight);
+            pictureBox1.Refresh();
+
+            int WidthInMM = Convert.ToInt16((pictureBox1.Size.Width - 1) /
+            g.DpiX * 25.4);
+            int HeightInMM = Convert.ToInt16((pictureBox1.Size.Height - 1) /
+            g.DpiY * 25.4);
+
+            g.DrawRectangle(axesPen, 0, 0, WidthInMM, HeightInMM - 0.5f);
+            g.DrawLine(axesPen, 0, HeightInMM / 2, WidthInMM, HeightInMM / 2);
+            g.DrawLine(axesPen, WidthInMM / 2, 0, WidthInMM / 2, HeightInMM);
+
+            //g.TranslateTransform((pictureBox1.Size.Width - 1) / 2,
+            //   (pictureBox1.Size.Height - 1) / 2);
+            g.TranslateTransform(WidthInMM / 2, HeightInMM / 2);
+            g.ScaleTransform(1, -1);
+
+            x = -40f;
+            while (x <= 40)
+            {
+                y = Convert.ToSingle(2 * Math.Pow(x, 3) + 2 * x);
+                g.DrawLine(graphicsPen, old_ex, old_ey,
+                    x / g.DpiX * 25.4f , y / g.DpiY * 25.4f);
+
+                old_ex = x;
+                old_ey = y;
+
+                x += 0.01f;
+            }
+
+            g.ScaleTransform(-1, 1);
+
+            g.Dispose();
+        }
+
+        private void Inches_Click(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+
+            int ex = 0, ey = 0;
+            float old_ex = 0, old_ey = 0;
+            float x = 0, y = 0;
+            float shag = 0;
+
+            g.PageUnit = GraphicsUnit.Inch;
+            Pen axesPen = new Pen(Color.Orange, 0.01f);
+            Pen graphicsPen = new Pen(Color.FromArgb(255, 0, 0), 0.01f);
+
+            pictureBox1.BackColor = Color.FromName("LightCyan");
+            pictureBox1.Refresh();
+
+            float WidthInInches = (pictureBox1.Size.Width - 1) / g.DpiX;
+            float HeightInInches = (pictureBox1.Size.Height - 1) / g.DpiY;
+
+            g.DrawRectangle(axesPen, 0, 0, WidthInInches, HeightInInches);
+            g.DrawLine(axesPen, 0, HeightInInches / 2, WidthInInches,
+                HeightInInches / 2);
+            g.DrawLine(axesPen, WidthInInches / 2, 0, WidthInInches / 2,
+                HeightInInches);
+
+            g.TranslateTransform(WidthInInches / 2, HeightInInches / 2);
+            g.ScaleTransform(1, -1);
+
+            x = -40f;
+            while (x <= 40)
+            {
+                y = Convert.ToSingle(2 * Math.Pow(x, 3) + 2 * x);
+                g.DrawLine(graphicsPen, old_ex, old_ey,
+                    x, y);
+
+                old_ex = x;
+                old_ey = y;
+
+                x += 0.01f;
+            }
+
+            g.ScaleTransform(-1, 1);
+
+            g.Dispose();
+        }
+    }
+}
